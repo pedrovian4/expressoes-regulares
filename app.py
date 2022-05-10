fca6ed5c8aaadb6b19d9b0dc791d236f80858861
Pedro <pedrovianaasking@gmail.com>
2022-05-10 19:50:37 -0300

    Todos os test de backend e front foram feitos

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template
+from re import M
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+symbols=('*','+','(',')','^')
+
+def generate(exp): 
+    exp_symbols:str
+    for i in symbols:
+        if i in exp: 
+            exp_symbols=exp 
+        
+
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index (): 
-    exp=''
-    return render_template('index.html',exp=exp)
+
+    
+    exp= request.form.get("Expression")
+    cadeias =['aaaaa','bbbbb','cccccc']
+    
+    
+    
+    return render_template('index.html',cadeias=cadeias)
