10c42e6610f1086a46f41df1650b5c415dfc0231
Pedro <pedrovianaasking@gmail.com>
2022-05-28 19:37:10 -0300

    operação estrela funcionando

--- exp/ula.py
+++ exp/ula.py
@@ -11,6 +11,11 @@ def indetifier(exps:list):
             if char == '^':
                 for plus_l in over_plus(exp[index-1]): 
                     language.add(exp[0:index-1]+plus_l)
+            if char == '*':
+                for star_l in star(exp[index-1]): 
+                    if star_l =='':
+                        star_l = '\u03BB'	
+                    language.add(exp[0:index-1]+star_l)
     return language
                  
                  
@@ -28,6 +33,6 @@ def over_plus(char):
 
 def star(char):
     exp=[]
-    for i in range(4):
+    for i in range(5):
         exp=[*exp, char*i]  
     return exp
